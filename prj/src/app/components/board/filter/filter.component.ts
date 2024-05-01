d1f79c7eb23fc233b2b206b2f546fa9c4bfdc42a
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-05-01 22:08:34 +0400

    modified table design & added filter design

--- prj/src/app/components/board/filter/filter.component.ts
+++ prj/src/app/components/board/filter/filter.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
   imports: [],
   templateUrl: './filter.component.html',
-  styleUrl: './filter.component.css'
+  styleUrl: './filter.component.css',
 })
 export class FilterComponent {
-
+  constructor() {}
 }
--- prj/src/app/components/board/table/table.component.ts
+++ prj/src/app/components/board/table/table.component.ts
@@ -1,15 +1,22 @@
-import { Component, NgZone, inject } from '@angular/core';
+import { Component, NgZone, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CardsService } from '../../../shared/services/cards/cards.service';
+import { card } from '../../../shared/model/cardModel';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css',
 })
 export class TableComponent {
-  CardsArr: [] = [];
+  CardsArr: card[] = [];
   isFilterFolded: boolean = false;
-  constructor() {}
+  constructor(private cardServ: CardsService) {}
+  ngOnInit() {
+    this.cardServ.getCards().subscribe((data: any) => {
+      this.CardsArr = data;
+    });
+  }
 }
